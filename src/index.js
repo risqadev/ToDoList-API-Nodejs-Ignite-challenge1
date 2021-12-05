3bcee32cc11148b68958a0772479803c1c4c6f6f
Ricardo Camacho <rscamachodev@gmail.com>
2021-12-04 21:08:44 -0300

    route POST '/users' complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const username = request.headers.username || request.body.username;
+
+  const user = users.find(user => user.username === username);
+
+  request.user = user;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users', checksExistsUserAccount, (request, response) => {
+  const { name, username } = request.body;
+  const { user } = request;
+
+  if (!!user) {
+    return response.status(400).json({ error: "Username already exists." })
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  };
+
+  users.push(newUser);
+
+  return response.json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
